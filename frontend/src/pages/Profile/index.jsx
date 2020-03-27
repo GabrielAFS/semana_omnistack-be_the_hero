2213d022a4bce50a0849f9234eab34b87601b926
Gabriel Augusto Fontes da Silva <gabriel_fsilva@outlook.com.br>
2020-03-27 12:48:24 -0300

    Mensagem informando que não há incidentes (front)

--- frontend/src/pages/Profile/index.jsx
+++ frontend/src/pages/Profile/index.jsx
@@ -62,29 +62,34 @@ export default function Profile() {
 
             <h1>Casos cadastrados</h1>
 
-            <ul>
-                {incidents.map(incident => (
-                    <li key={incident.id}>
-                        <strong>CASO: </strong>
-                        <p>{incident.title}</p>
+            { (incidents.length) 
+                ? <ul>
+                    {
+                        incidents.map(incident => (
+                            <li key={incident.id}>
+                                <strong>CASO: </strong>
+                                <p>{incident.title}</p>
 
-                        <strong>DESCRIÇÃO:</strong>
-                        <p>{incident.description}</p>
+                                <strong>DESCRIÇÃO:</strong>
+                                <p>{incident.description}</p>
 
-                        <strong>VALOR:</strong>
-                        <p>
-                            {Intl.NumberFormat(
-                                'pt-BR',
-                                { style: 'currency', currency: 'BRL' }
-                            ).format(incident.value)}
-                        </p>
+                                <strong>VALOR:</strong>
+                                <p>
+                                    {Intl.NumberFormat(
+                                        'pt-BR',
+                                        { style: 'currency', currency: 'BRL' }
+                                    ).format(incident.value)}
+                                </p>
 
-                        <button onClick={() => handleDeleteIncident(incident.id)} type="button">
-                            <FiTrash2 size={20} color="a8a8b3" />
-                        </button>
-                    </li>
-                ))}
-            </ul>
+                                <button onClick={() => handleDeleteIncident(incident.id)} type="button">
+                                    <FiTrash2 size={20} color="a8a8b3" />
+                                </button>
+                            </li>
+                        ))
+                    }
+                </ul>
+                : <p className="empty-cases">Nenhum caso cadastrado até então :(</p>
+            }
         </div>
     )
 }
