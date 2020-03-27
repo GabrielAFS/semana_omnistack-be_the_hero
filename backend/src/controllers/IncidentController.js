e4d5563ba2023c687c8101933dc1f996fbc60289
Gabriel Augusto Fontes da Silva <gabriel_fsilva@outlook.com.br>
2020-03-27 12:47:13 -0300

    Correção da listagem de incidentes (back)

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -17,10 +17,12 @@ module.exports = {
 
     async list(req, res) {
         const { page = 1 } = req.query // Parâmetro de paginação
+        const ong_id = req.headers.authorization
 
         const [count] = await connection('incidents').count()
         const incidents = await connection('incidents')
             .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+            .where('ongs.id', '=', ong_id)
             .limit(5)
             .offset((page - 1) * 5)
             .select([
